168d943eca7f6985345b002921b5876129fd6d11
Xfree <xfreeming@gmail.com>
2024-11-28 16:18:22 +0800

    del lib tests Please enter the commit message for your changes. Lines starting

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,3 @@ mod two_sum_1;
 mod valid_parentheses_20;
 #[allow(dead_code)]
 struct Solution;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
